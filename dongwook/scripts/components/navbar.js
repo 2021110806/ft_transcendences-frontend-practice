a6d3de5f649725a9fa8c313c0f8372858e843305
Dongwoo Kim <dongwookim@Dongwooui-MacBookAir.local>
2025-01-26 18:18:32 +0900

    gam option ui까지

--- dongwook/scripts/components/navbar.js
+++ dongwook/scripts/components/navbar.js
@@ -4,10 +4,31 @@ navbar.innerHTML = `
   <nav>
     <div>
       <div>
-        <img src="icon.png" alt="Logo" style="height: 30px; margin-right: 10px;">
-        <a href="#profile">Profile</a>
-        <a href="#gameplay">GamePlay</a>
+        <img src="../static/logo.png" alt="Logo">
+        <a href="#profile" class="nav-link">Profile</a>
+        <a href="#gameplay" class="nav-link">GamePlay</a>
       </div>
     </div>
   </nav>
 `;
+
+function updateActiveLink() {
+  const currentHash = window.location.hash;
+  document.querySelectorAll('.nav-link').forEach(link => {
+    if (link.getAttribute('href') === currentHash) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+updateActiveLink();
+window.addEventListener('hashchange', updateActiveLink);
+
+
+
+// const nextButton = document.getElementById('next-button');
+// nextButton.addEventListener('click', () => {
+//   window.location.hash = '#gameplay'; // 특정 해시로 이동
+// });
